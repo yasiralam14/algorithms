e08f4cfad2560811151d3bc1012995e6514c67ca
yasiralam14 <yasir.alam14@gmail.com>
2025-03-09 18:02:04 -0400

    karatsuba multiply

--- multiplication.py
+++ multiplication.py
@@ -1,4 +1,5 @@
 import math
+import random
 def karatsuba_multiply(num1,num2):
     
     num1 = str(num1)
@@ -27,16 +28,37 @@ def karatsuba_multiply(num1,num2):
     c = num2[0:half_length_2]
     d = num2[half_length_2:]
    
-    ac = karatsuba_multiply(int(a),int(c))
-    bd = karatsuba_multiply(int(b),int(d))
-    ad = karatsuba_multiply(int(a),int(d))
-    bc = karatsuba_multiply(int(b),int(c))
-    middle = ad + bc
-    result = 10**(num1_length)*ac + 10**(num1_length/2)*middle + bd
+    ac = karatsuba_multiply(a,c)
+    bd = karatsuba_multiply(b,d)
+    a_plus_b = int(a)+int(b)
+    c_plus_d = int(c)+int(d)
+    multiple = karatsuba_multiply(a_plus_b,c_plus_d)
+    middle = multiple- int(ac) - int(bd)
+    n= len(num1)
+    n_by_2 = int(n/2)
+    result = int(10**(n)*ac) + int(10**(n_by_2)*middle) + int(bd)
     
-    return int(result)
-    
-print(karatsuba_multiply(200,30))
-    
-print(karatsuba_multiply(3141592653589793238462643383279502884197169399375105820974944592,2718281828459045235360287471352662497757247093699959574966967627))
+    return result
+
+
+list_of_numbers = []
+for i in range(6):
+    digits = 2**(i+1)
+    first_range = int('1'+'0'*(digits-1))
+    second_range = int('9'*digits)
+    for j in range(10):
+        number1 = random.randint(first_range,second_range)
+        number2 = random.randint(first_range,second_range)
+        list_of_numbers.append((number1,number2))
+        
+for pair in list_of_numbers:
+    test_result = karatsuba_multiply(pair[0],pair[1])
+    python_result = pair[0]*pair[1]
+    if test_result != python_result:
+        print(f"The first number is {pair[0]} and the second number is {pair[1]}")
+        print(f"What we got {test_result}")
+        print(f"what python got {python_result}")
+        print(f"Our length {len(str(test_result))}")
+        print(f"Python length {len(str(python_result))}")
+        break
     
